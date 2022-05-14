8c8c79c369ce8ba87fc9e8c44b1e435eda7f7cd2
franklinBen <murod194532@gmail.com>
2022-05-14 14:37:53 +0500

    final

--- task03-map.go
+++ task03-map.go
@@ -2,14 +2,15 @@ package homework
 
 import "sort"
 
-func sortMapValues(input map[int]string) (result []string) {
-	i := 1
+func sortMapValues(input map[int]string) []string {
+	i := 0
 	keys := make([]int, len(input))
 	for k := range input {
 		keys[i] = k
 		i++
 	}
 	sort.Ints(keys)
+	result := make([]string, len(input))
 	for _, m := range keys {
 		result[m] = input[m]
 	}
